9dfdec111410302f71a93fd46e1610df8271e272
Henrik Larsson <henlar.work@gmail.com>
2018-03-01 10:13:43 +0100

    Add Magnitude enums for Block, change colormap to reflect this

--- src/Block.java
+++ src/Block.java
@@ -2,28 +2,34 @@ import java.util.Random;
 
 public class Block {
 
-    public int currentValue;
+    public enum Magnitude {
+        M2, M4, M8, M16, M32, M64, M128, M256, M512, M1024, M2048;
+
+        private static Magnitude[] values = values();
+        public Magnitude next() {
+            return values[(this.ordinal()+1) % values.length]; // %length to loop around, might be undesired behaviour.
+        }
+    }
+
+    public Magnitude currentMagnitude;
     private Position position;
     private RGBColor color;
     private boolean newlyCombined = false;
     private static Random random = new Random();
 
     public Block(Position position) {
-        currentValue = ((random.nextInt(2) + 1) * 2); // initial value set to 2 or 4
-        this.position = position;
-        this.color = ColorMap.getColor(currentValue);
-
+        this(position, (random.nextInt(2) == 0) ? Magnitude.M2 : Magnitude.M4);
     }
 
-    public Block(Position position, int currentValue) {
-        this.currentValue = currentValue;
+    public Block(Position position, Magnitude currentMagnitude) {
+        this.currentMagnitude = currentMagnitude;
         this.position = position;
-        this.color = ColorMap.getColor(currentValue);
+        this.color = ColorMap.getColor(currentMagnitude);
     }
 
     //Debug
     public void print() {
-        System.out.println("Block\t" + "Position " + position.getX() + ", " + position.getY() + "\tcurrentValue " + currentValue);
+        System.out.println("Block\t" + "Position " + position.getX() + ", " + position.getY() + "\tcurrentValue " + currentMagnitude);
     }
 
     public Position getPosition() {
--- src/ColorMap.java
+++ src/ColorMap.java
@@ -1,24 +1,25 @@
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class ColorMap {
-    private static final Map<Integer, RGBColor> colorMap = new HashMap<>();
+    private static final Map<Block.Magnitude, RGBColor> colorMap = new HashMap<>();
 
     static {
-        colorMap.put(2, new RGBColor(41,165,0));
-        colorMap.put(4, new RGBColor(110, 237, 68));
-        colorMap.put(8, new RGBColor(23,44,233));
-        colorMap.put(16, new RGBColor( 149, 204, 237));
-        colorMap.put(32, new RGBColor(249, 24, 216 ));
-        colorMap.put(64, new RGBColor(249, 167, 237 ));
-        colorMap.put(128, new RGBColor( 255, 17, 17));
-        colorMap.put(256, new RGBColor( 237, 104, 2));
-        colorMap.put(512, new RGBColor( 76, 0, 57));
-        colorMap.put(1024, new RGBColor( 192,192,192));
-        colorMap.put(2048, new RGBColor(255, 215, 0));
+        colorMap.put(Block.Magnitude.M2, new RGBColor(41,165,0));
+        colorMap.put(Block.Magnitude.M4, new RGBColor(110, 237, 68));
+        colorMap.put(Block.Magnitude.M8, new RGBColor(23,44,233));
+        colorMap.put(Block.Magnitude.M16, new RGBColor( 149, 204, 237));
+        colorMap.put(Block.Magnitude.M32, new RGBColor(249, 24, 216 ));
+        colorMap.put(Block.Magnitude.M64, new RGBColor(249, 167, 237 ));
+        colorMap.put(Block.Magnitude.M128, new RGBColor( 255, 17, 17));
+        colorMap.put(Block.Magnitude.M256, new RGBColor( 237, 104, 2));
+        colorMap.put(Block.Magnitude.M512, new RGBColor( 76, 0, 57));
+        colorMap.put(Block.Magnitude.M1024, new RGBColor( 192,192,192));
+        colorMap.put(Block.Magnitude.M2048, new RGBColor(255, 215, 0));
     }
 
-    public static RGBColor getColor(int key) {
+    public static RGBColor getColor(Block.Magnitude key) {
         return colorMap.get(key);
     }
 }
